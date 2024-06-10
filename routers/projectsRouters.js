c8555b221e2e374f7113942670035c75f4a9b210
turkifahd1 <jjbh2710@gmail.com>
2024-06-10 14:10:20 +0300

    abdate

--- routers/projectsRouters.js
+++ routers/projectsRouters.js
@@ -1,19 +1,19 @@
 const express=require('express')
-const Routerr=express.Router();
+const routerr=express.Router();
 const projectsControllers =require("../controllers/projectsControllers")
 const projectsValidator = require('../validator/newsValidator')
 
 //post data to  mongo
-Routerr.post("/",projectsValidator.userValidationRules(),projectsValidator.validate,projectsControllers.post)
+routerr.post("/",projectsValidator.userValidationRules(),projectsValidator.validate,projectsControllers.post)
 //Get all data to mongo
-Routerr.get("/",projectsControllers.get)
+routerr.get("/",projectsControllers.get)
 
 //Get data by id 
-Routerr.get("/:id",projectsControllers.getID)
+routerr.get("/:id",projectsControllers.getID)
 
 //delete data by id 
-Routerr.delete("/:id",projectsControllers.delete)
+routerr.delete("/:id",projectsControllers.delete)
 
 //abdate data by id 
-Routerr.put("/:id",projectsValidator.userValidationRules(),projectsValidator.validate ,projectsControllers.update)
-module.exports= Routerr;
+routerr.put("/:id",projectsValidator.userValidationRules(),projectsValidator.validate ,projectsControllers.update)
+module.exports= routerr;
